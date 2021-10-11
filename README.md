9e67d8384afd2932be8e168fa3c85c0b39257a89
priyasharma12003 <89292659+priyasharma12003@users.noreply.github.com>
2021-10-11 18:05:14 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+<div>
+  <br/><br />
+  <p align="center">
+    <a href="https://bambuser.com" target="_blank" align="center">
+        <img src="https://brand.bambuser.net/current/logo/bambuser-black-512.png" width="280">
+    </a>
+  </p>
+  <br /><br />
+  <h1>Bambuser broadcast SDK for Android sample code</h1>
+</div>
+
+Sample code for broadcasting using the Bambuser broadcast SDK for Android.
+
+The sample code in this project contains the bare minimum required to start broadcasting.
+
+
+## Getting started
+
+1. Install Android Studio.
+2. Clone the sample code from this repository, so you have a local directory named i.e. `ExampleBroadcaster`.
+3. Sign up for an account on https://bambuser.com if you don't already have one.
+4. Download the Android SDK zip from https://dashboard.bambuser.com/developer and open the zip file.
+5. Copy the `libbambuser-x.x.x.aar` file to the `ExampleBroadcaster/libbambuser` folder.
+6. Import the `ExampleBroadcaster` project in Android Studio.
+7. Edit `ExampleBroadcaster/libbambuser/build.gradle` and point it to the version of the `.aar` file.
+8. Generate an `applicationId` at https://dashboard.bambuser.com/developer
+9. Open the `MainActivity.java` file in Android Studio and insert the `applicationId` generated above.
+10. Run the sample code on your device and start a broadcast. The broadcast should show up on https://dashboard.bambuser.com/content
+
+## More information
+
+* [Bambuser Docs](https://bambuser.com/docs)
+
+* [Bambuser AB](https://bambuser.com)
+
+* Our guide for building a broadcaster app from scratch can be found at [Broadcaster guide](https://bambuser.com/docs/broadcasting/android/).
---
+++ app/src/main/java/com/example/mybroadcastingapp/MainActivity.java
@@ -0,0 +1,118 @@
+package com.example.mybroadcastingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import com.bambuser.broadcaster.BroadcastStatus;
+import com.bambuser.broadcaster.Broadcaster;
+import com.bambuser.broadcaster.CameraError;
+import com.bambuser.broadcaster.ConnectionError;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String LOGTAG = "Mybroadcastingapp";
+
+    private static final String APPLICATION_ID = "za9MdaeCgpBb9AMVCrRvDw";
+    SurfaceView mPreviewSurface;
+    Broadcaster mBroadcaster;
+    Button mBroadcastButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mPreviewSurface = findViewById(R.id.PreviewSurfaceView);
+        mBroadcaster = new Broadcaster(this, APPLICATION_ID, mBroadcasterObserver);
+        mBroadcaster.setRotation(getWindowManager().getDefaultDisplay().getRotation());
+        mBroadcastButton = findViewById(R.id.BroadcastButton);
+        mBroadcastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mBroadcaster.canStartBroadcasting())
+                    mBroadcaster.startBroadcast();
+                else
+                    mBroadcaster.stopBroadcast();
+            }
+        });
+
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mBroadcaster.onActivityDestroy();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        mBroadcaster.onActivityPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (!hasPermission(Manifest.permission.CAMERA)
+                && !hasPermission(Manifest.permission.RECORD_AUDIO))
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA,
+                    Manifest.permission.RECORD_AUDIO}, 1);
+        else if (!hasPermission(Manifest.permission.RECORD_AUDIO))
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.RECORD_AUDIO}, 1);
+        else if (!hasPermission(Manifest.permission.CAMERA))
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 1);
+        mBroadcaster.setCameraSurface(mPreviewSurface);
+        mBroadcaster.onActivityResume();
+        mBroadcaster.setRotation(getWindowManager().getDefaultDisplay().getRotation());
+
+    }
+
+    private boolean hasPermission(String permission) {
+        return ActivityCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private Broadcaster.Observer mBroadcasterObserver = new Broadcaster.Observer() {
+        @Override
+        public void onConnectionStatusChange(BroadcastStatus broadcastStatus) {
+            Log.i(LOGTAG, "Received status change: " + broadcastStatus);
+            if (broadcastStatus == BroadcastStatus.STARTING)
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            if (broadcastStatus == BroadcastStatus.IDLE)
+                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            mBroadcastButton.setText(broadcastStatus == BroadcastStatus.IDLE ? "Broadcast" : "Disconnect");
+        }
+        @Override
+        public void onStreamHealthUpdate(int i) {
+        }
+        @Override
+        public void onConnectionError(ConnectionError connectionError, String s) {
+            Log.w(LOGTAG, "Received connection error: " + connectionError + ", " + s);
+        }
+        @Override
+        public void onCameraError(CameraError cameraError) {
+            Log.w(LOGTAG, "Received camera error: " + cameraError);
+        }
+        @Override
+        public void onChatMessage(String s) {
+        }
+        @Override
+        public void onResolutionsScanned() {
+        }
+        @Override
+        public void onCameraPreviewStateChanged() {
+        }
+        @Override
+        public void onBroadcastInfoAvailable(String s, String s1) {
+        }
+        @Override
+        public void onBroadcastIdAvailable(String s) {
+        }
+    };
+
+
+
+}
